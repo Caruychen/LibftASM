1964cfecad56db9fe8fdce410a1b5be7af6d6aa2
Caruy Chen <cchen@student.hive.fi>
2022-11-21 14:02:50 +0200

    set up test framework

---
+++ test/src/test_ft_strlen.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_strlen.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 13:47:49 by cchen             #+#    #+#             */
+/*   Updated: 2022/11/21 13:47:50 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libfts.h"
+#include <stdio.h>
+#include <string.h>
+
+void	test_ft_strlen(void)
+{
+	char	*s;
+
+	s = "Hello World";
+	printf("Test: %s, strlen: %zu, ft_strlen: %zu\n", s, strlen(s), ft_strlen(s));
+}
