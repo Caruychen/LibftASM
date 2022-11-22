4a32bf110343a66558fb51993dacfec2119e7705
Caruy Chen <cchen@student.hive.fi>
2022-11-22 15:08:40 +0200

    add ft_islower and add headers

--- src/ft_islower.s
+++ src/ft_islower.s
@@ -1,28 +1,13 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
+#    ft_islower.s                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/11/22 15:08:14 by cchen             #+#    #+#              #
+#    Created: 2022/11/22 15:08:26 by cchen             #+#    #+#              #
 #    Updated: 2022/11/22 15:08:28 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-section	.text
-	global	_ft_strlen
 
-_ft_strlen:
-	mov	rax, 0
-
-compare:
-	cmp	BYTE [rdi + rax], 0
-	je	end
-
-increment:
-	inc	rax
-	jmp	compare
-
-end:
-	ret
