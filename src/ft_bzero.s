d433ee3f5c82dc55261fcfdc0bcb464a0103b0de
Caruy Chen <caruychen@gmail.com>
2022-11-22 10:14:44 +0200

    optimise bzero

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -4,9 +4,22 @@ section	.text
 _ft_bzero:
 	mov	rax, 0					; i = 0
 
+null_guard:						; if (*s == NULL) { return }
+	cmp	rdi, 0
+	je	end
+
 while:							; while (i < n)
 	cmp	rax, rsi
 	jae	end
+	mov	rbx, rsi				; if (i >= 8) ( bzero 1 byte at a time)
+	sub	rbx, rax
+	cmp	rbx, 8
+	jb	set_zero
+
+large_bzero:						; bzero in 64bits
+	mov	QWORD [rdi + rax], 0
+	add	rax, 8
+	jmp	while
 
 set_zero:
 	mov	BYTE [rdi + rax], 0
