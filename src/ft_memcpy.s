306f7ed1e2c1cb1f7a296ebbbd68b0630c20dbd5
Caruy Chen <cchen@student.hive.fi>
2022-11-24 15:38:27 +0200

    use REP in ft_memcpy

--- src/ft_memcpy.s
+++ src/ft_memcpy.s
@@ -2,37 +2,24 @@ section	.text
 	global	_ft_memcpy
 
 _ft_memcpy:
-__guard:
-	mov	rax, rdi
-	test	rdx, rdx	; if (n == 0) { return }
-	jz	end
-	test	rdi, rdi	; if (dst == NULL) { return }
-	jz	end
-	test	rsi, rsi	; if (src == NULL) { return }
-	jz	end
-	cmp	rdi, rsi	; if (dst == src) { return }
-	je	end
-	mov	rbx, 0		; index = 0;
+guard:
+	MOV		rax, rdi
+	TEST	rdx, rdx	; if (n == 0) { return }
+	JZ		end
+	TEST	rdi, rdi	; if (dst == NULL) { return }
+	JZ		end
+	TEST	rsi, rsi	; if (src == NULL) { return }
+	JZ		end
+	CMP		rdi, rsi	; if (dst == src) { return }
+	JE		end
+	MOV		rbx, rsi
 
-while:
-	cmp	rbx, rdx	; while ( index < n )
-	jae	end
-	mov	r8, rdx		; if ( n - index > 8 ) { copy 64bits at a time }
-	sub	r8, rbx
-	cmp	r8, 8
-	jb	small_cpy
-
-large_cpy:
-	mov	rcx, QWORD [rsi + rbx]
-	mov	QWORD [rdi + rbx], rcx
-	add	rbx, 8
-	jmp	while
-
-small_cpy:
-	mov	cl, BYTE [rsi + rbx]
-	mov	BYTE [rdi + rbx], cl
-	inc	rbx
-	jmp	while
+cpy_bytes:				; Potential to optimise to copy in QWORDs
+	CLD
+	MOV		rcx, rdx
+	REP		MOVSB
+	MOV		rdi, rax
+	MOV		rsi, rbx
 
 end:
 	ret
