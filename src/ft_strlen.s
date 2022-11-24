bebf133f8ab05bb50671d83bea68f24cecedf2ff
Caruy Chen <cchen@student.hive.fi>
2022-11-24 15:03:36 +0200

    use repnz in ft_strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -2,15 +2,18 @@ section	.text
 	global	_ft_strlen
 
 _ft_strlen:
-	mov	rax, 0
+	mov		al, 0				; Set search for '\0'
+	mov		rbx, rdi			; Store pointer to string
 
-compare:
-	cmp	BYTE [rdi + rax], 0
-	je	end
+repeat_scan:
+	mov		ecx, 0xffffffff		; repnz uses cx to count loop
+	cld							; clear direction flag, to scan forward
+	repnz	scasb				; repeat scan until cx == 0 or zf == 0
+	jnz		repeat_scan			; loop again if '\0' not found
 
-increment:
-	inc	rax
-	jmp	compare
-
-end:
+return_result:
+	mov		rax, rdi
+	sub		rax, rbx
+	dec		rax					; end ptr [rdi] - start ptr [rbx] + 1
+	mov		rdi, rbx			; restore rdi
 	ret
