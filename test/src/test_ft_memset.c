4cffa7cfe10276a9b42926e41de2db018fdde9f3
Caruy Chen <cchen@student.hive.fi>
2022-11-25 15:23:39 +0200

    set up ft_memset test

--- test/src/test_ft_memset.c
+++ test/src/test_ft_memset.c
@@ -10,4 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	test_ft_memset(void);
+#include "libfts.h"
+#include <limits.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+static void	init_str(char **str, size_t size)
+{
+	*str = (char *) malloc(sizeof(char) * size);
+	bzero(*str, size);
+}
+
+static void	cycle_ints(char *control, char *test, size_t index, size_t size)
+{
+	int	c;
+
+	c = SHRT_MIN;
+	while (c <= SHRT_MAX)
+	{
+		memset(control, c, index);
+		ft_memset(test, c, index);
+		assert(memcmp(control, test, size) == 0);
+		++c;
+	}
+}
+
+static void	run_test(char *control, char *test, size_t size)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < size)
+	{
+		bzero(control, size);
+		bzero(test, size);
+		cycle_ints(control, test, index++, size);
+	}
+}
+
+void	test_ft_memset(void)
+{
+	char	*control;
+	char	*test;
+	size_t	size;
+
+	printf("ft_memset: ");
+	size = 42;
+	init_str(&control, size);
+	init_str(&test, size);
+	run_test(control, test, size);
+	free(control);
+	free(test);
+	printf("OK\n");
+}
