d87704f86ba44892cb939276375a26505e0d81d6
Caruy Chen <caruychen@gmail.com>
2022-11-28 11:28:19 +0200

    buggy ft_strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -3,6 +3,27 @@ section	.text
 
 extern	_ft_strlen
 extern	_ft_memcpy
+extern	_malloc
 
 _ft_strdup:
-	call	_ft_strlen
+	PUSH	rbp
+	MOV	rbp, rsi
+	PUSH	rdi		; Preserve source location
+
+allocate_heap:			; Calculates src length and allocates memory
+	CALL	_ft_strlen
+	MOV	rdi, rax
+	INC	rdi
+	PUSH	rdi		; preserve string length
+	CALL	_malloc
+
+copy_string:			; calls ft_memcpy to copy string over
+	POP	rdx
+	POP	rsi
+	MOV	rdi, rax
+	CALL	_ft_memcpy
+	MOV	rdi, rsi
+
+end:
+	LEAVE
+	RET
