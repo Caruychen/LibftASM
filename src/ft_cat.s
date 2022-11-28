e86f8dc15c0998beda670650cca3f1b816d11444
Caruy Chen <cchen@student.hive.fi>
2022-11-28 15:24:24 +0200

    ft_cat

--- src/ft_cat.s
+++ src/ft_cat.s
@@ -7,32 +7,55 @@
 
 section	.data
 error:
-	.string	db "No such file or directory", NEWLINE
-	.len	equ $ - error.string
+	.nofile	db "No such file or directory", 0
+	.read db "Could not read file", 0
+	.write db "Could not write to file", 0
 
 section	.text
 	global _ft_cat
+
+extern	_ft_puts
 
 _ft_cat:
 	PUSH	rbp
 	MOV		rbp, rsp
 	SUB		rsp, BUFFSIZE
 	MOV		rsi, rsp
-	MOV		rbx, rdi
+
+check_fd:
+	MOV		ebx, edi
+	CMP		edi, 0
+	JNL		read_fd
+	LEA		rdi, [rel error.nofile]
+	CALL	_ft_puts
+	JMP		end
+
+read_error:
+	LEA		rdi, [rel error.read]
+	CALL	_ft_puts
+	JMP		end
+
+write_error:
+	LEA		rdi, [rel error.write]
+	CALL	_ft_puts
+	JMP		end
 
 read_fd:
-	MOV		rdi, rbx
+	MOV		edi, ebx
 	MOV		rdx, BUFFSIZE
 	MOV		rax, MACH_SYSCALL(READ)
 	SYSCALL
 	TEST	rax, rax
 	JZ		end
+	JS		read_error
 
 write_stdout:
-	MOV		rdi, STDOUT
+	MOV		edi, STDOUT
 	MOV		rdx, rax
 	MOV		rax, MACH_SYSCALL(WRITE)
 	SYSCALL
+	TEST	rax, rax
+	JS		write_error
 	JMP		read_fd
 
 end:
